cc1c8aca917e233415336eb1bc9f1bafefee556c
valeriapopova <iamvaleriapopova@yandex.ru>
2022-09-02 14:58:58 +0400

    for sheets

---
+++ config_example.py
@@ -0,0 +1,23 @@
+""" Токен вк """
+token = ''
+
+''' Авторизационные данные для google sheets'''
+auth_dict = {
+      "type": "service_account",
+      "project_id": "ozonsheets",
+      "private_key_id": "",
+      "private_key": "-----BEGIN PRIVATE KEY-----\n\n-----END PRIVATE KEY-----\n",
+      "client_email": "account@ozonsheets.iam.gserviceaccount.com",
+      "client_id": "",
+      "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+      "token_uri": "https://oauth2.googleapis.com/token",
+      "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+      "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/account%40ozonsheets.iam.gserviceaccount.com",
+      "spreadsheetId": ""
+             }
+
+age_from = 22
+age_to = 60
+sex = 1
+city = 'Самара'
+count = 5
--- main.py
+++ main.py
@@ -1,29 +1,37 @@
-import json
 from pprint import pprint
+
 from config import *
 import requests as requests
 
-age_from = 18
-age_to = 20
-sex = 1
-city = 'москва'
-count = 20
 
 url_for_vk = 'http://127.0.0.1:5000/vk/search_users'
 data_for_vk = {'token': token, 'age_from': age_from, 'age_to': age_to, 'sex': sex, 'city': city, 'count': count}
 r = requests.post(url_for_vk, data=data_for_vk)
 res = r.json()
 res.update(auth_dict)
-# pprint(res)
-headers = {"Content-Type": "application/json"}
-# url_for_sheets = 'http://127.0.0.1:5001/sheets/json'
-# response = requests.post(url_for_sheets, headers=headers, data=res)
-# print(response)
+pprint(res)
 
-with open('example.json', 'w') as file:
-    json.dump(res, file, ensure_ascii=False, indent=3)
+''' Добавляет только значения в google sheets'''
+url_for_sheets = 'http://127.0.0.1:5001/sheets/append_values'
+response = requests.post(url_for_sheets, json=res)
+print(response)
 
+''' Добавляет данные в таблицу google sheets '''
+url_for_sheets = 'http://127.0.0.1:5001/sheets/append'
+response = requests.post(url_for_sheets, json=res)
+print(response)
 
-# url_for_excel = 'http://127.0.0.1:5001/excel'
-# response = requests.post(url_for_excel, data='example.json')
-# print(response)
+''' Перезаписывает данные в таблицу google sheets   '''
+url_for_sheets = 'http://127.0.0.1:5001/sheets/clear_append'
+response = requests.post(url_for_sheets, json=res)
+print(response)
+
+''' Добавляет данные в новый лист google sheets  '''
+url_for_sheets = 'http://127.0.0.1:5001/sheets/append_list'
+response = requests.post(url_for_sheets, json=res)
+print(response)
+
+''' Добавляет данные в excel  '''
+url_for_excel = 'http://127.0.0.1:5001/excel'
+response = requests.post(url_for_excel, json=res)
+print(response)

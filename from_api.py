50a3f3b4f04b550427b97739b141be1bd5f0f8a2
valeriapopova <iamvaleriapopova@yandex.ru>
2022-09-26 18:16:03 +0400

    add vk api new methods

--- from_api.py
+++ from_api.py
@@ -1,5 +1,7 @@
+import json
+
 from config import *
-# import requests
+import requests
 #
 #
 # def get_search_data_from_vk(token, age_from, age_to, sex, city , count, host='localhost'):
@@ -21,7 +23,62 @@ from config import *
 # def get_leads_bitrix(self, host='localhost'):
 #     """ Забирает новые лиды из Birix24  """
 #     url = f'http://{host}:5001/bitrix/get_leads'
-#     r = requests.post(url, json=json.loads(self.auth_from))
+#     r = requests.post(url, json=json.loads(auth_from))
 #     res = r.json()
 #     return res
-#
+
+
+
+#VkAds
+
+
+def get_statistic(host='localhost'):
+    """Возвращает статистику показателей эффективности по рекламным объявлениям,
+        кампаниям, клиентам или всему кабинету."""
+    url = f'http://{host}:5000/vk/ads_get_statistic'
+    r = requests.post(url, json=json.loads(auth_from))
+    res = r.json()
+    return res
+
+
+def get_demographics(host='localhost'):
+    """Возвращает демографическую статистику по рекламным объявлениям или кампаниям."""
+    url = f'http://{host}:5000/vk/ads_get_demographics'
+    r = requests.post(url, json=json.loads(auth_from))
+    res = r.json()
+    return res
+
+
+def get_targeting(host='localhost'):
+    """Возвращает параметры таргетинга рекламных объявлений"""
+    url = f'http://{host}:5000/vk/ads_get_targeting'
+    r = requests.post(url, json=json.loads(auth_from))
+    res = r.json()
+    return res
+
+
+def get_targeting_stats(host='localhost'):
+    """Возвращает размер целевой аудитории таргетинга, а также рекомендованные
+       значения CPC и CPM."""
+    url = f'http://{host}:5000/vk/ads_get_targeting_stats'
+    r = requests.post(url, json=json.loads(auth_from))
+    res = r.json()
+    return res
+
+
+def get_posts_reach(host='localhost'):
+    """Возвращает подробную статистику по охвату рекламных записей из объявлений и кампаний для
+        продвижения записей сообщества."""
+    url = f'http://{host}:5000/vk/ads_get_posts_reach'
+    r = requests.post(url, json=json.loads(auth_from))
+    res = r.json()
+    return res
+
+
+def get_flood_stats(host='localhost'):
+    """Возвращает подробную статистику по охвату рекламных записей из объявлений и кампаний для
+        продвижения записей сообщества."""
+    url = f'http://{host}:5000/vk/ads_get_flood_stats'
+    r = requests.post(url, json=json.loads(auth_from))
+    res = r.json()
+    return res
